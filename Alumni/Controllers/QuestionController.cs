7d02ae5343d4117f5bfbb69a97bd1f3206a7606d
ivy <2023180742@qq.com>
2023-01-13 15:05:32 +0800

    完成问卷调查模块

---
+++ Alumni/Controllers/QuestionController.cs
@@ -0,0 +1,49 @@
+﻿using Alumni.Db;
+using Alumni.Models.Question;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alumni.Models;
+
+namespace Alumni.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public ActionResult QuestionIndex()
+        {
+            return View();
+        }
+
+        public ActionResult QuestionSave(QuestionModel model)
+        {
+            try
+            {
+                using (SchoolDb db = new SchoolDb())
+                {
+                    model.QchSeqNo = "QUE" + Utils.Nmrandom();
+                    model.CreateTime = DateTime.Now;
+                    model.Form_Name = "问卷调查";
+                    string insertSql2 = string.Format(@"INSERT INTO [db_forminf].[dbo].[Questionnaire _Investigation] 
+                                                               (QchSeqNo,Form_Name,Stu_Empno,Stu_Name,Stu_Eame,GraduationYear,CurrentDevelopment,CurrentDevelopmentText,WorkUnitName,
+                                                               JobTitle,ContentOverview,HighestEducation,HighestEducationStatus,HighestEducationText,UniversityName,UniversityDepartmentName,
+                                                               InstituteName,InstituteDepartmentName,DoctoralClassName,DoctoralDepartmentName,Transferred,TransferredText,OtherSupplements,CreateTime)
+                                                                VALUES(@QchSeqNo,@Form_Name,@Stu_Empno,@Stu_Name,@Stu_Eame,@GraduationYear,@CurrentDevelopment,@CurrentDevelopmentText,@WorkUnitName,
+                                                               @JobTitle,@ContentOverview,@HighestEducation,@HighestEducationStatus,@HighestEducationText,@UniversityName,@UniversityDepartmentName,
+                                                               @InstituteName,@InstituteDepartmentName,@DoctoralClassName,@DoctoralDepartmentName,@Transferred,@TransferredText,@OtherSupplements,@CreateTime)");
+                    Dictionary<string, object> trans = new Dictionary<string, object>();
+                    trans.Add(insertSql2, model);
+                    db.DoExtremeSpeedTransaction(trans);
+                }
+                return Json(new FlagTips { IsSuccess = true });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new FlagTips { IsSuccess = false, Msg = ex.Message });
+            }
+        }
+    }
+}
---
+++ Alumni/Models/Question/QuestionModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alumni.Models.Question
+{
+    public class QuestionModel
+    {
+        /// <summary>
+        /// 问卷流水号
+        /// </summary>
+        public string QchSeqNo { get; set; }
+
+        /// <summary>
+        /// 表单名称
+        /// </summary>
+        public string Form_Name { get; set; }
+
+        /// <summary>
+        /// 康桥学号
+        /// </summary>
+        public string Stu_Empno { get; set; }
+
+        /// <summary>
+        /// 中文名
+        /// </summary>
+        public string Stu_Name { get; set; }
+
+        /// <summary>
+        /// 英文名
+        /// </summary>
+        public string Stu_Eame { get; set; }
+
+        /// <summary>
+        /// 康桥毕业年份
+        /// </summary>
+        public string GraduationYear { get; set; }
+
+        /// <summary>
+        /// 目前发展状况
+        /// </summary>
+        public string CurrentDevelopment { get; set; }
+
+        /// <summary>
+        /// 目前发展状况其他说明
+        /// </summary>
+        public string CurrentDevelopmentText { get; set; }
+
+        /// <summary>
+        /// 工作单位名称
+        /// </summary>
+        public string WorkUnitName { get; set; }
+
+        /// <summary>
+        /// 工作职称
+        /// </summary>
+        public string JobTitle { get; set; }
+
+        /// <summary>
+        /// 内容概述
+        /// </summary>
+        public string ContentOverview { get; set; }
+
+        /// <summary>
+        /// 最高学历
+        /// </summary>
+        public string HighestEducation { get; set; }
+
+        /// <summary>
+        /// 最高学历状态
+        /// </summary>
+        public string HighestEducationStatus { get; set; }
+
+        /// <summary>
+        /// 最高学历状态其他说明
+        /// </summary>
+        public string HighestEducationText { get; set; }
+
+        /// <summary>
+        /// 大学名称
+        /// </summary>
+        public string UniversityName { get; set; }
+
+        /// <summary>
+        /// 大学科系名称
+        /// </summary>
+        public string UniversityDepartmentName { get; set; }
+
+        /// <summary>
+        /// 研究所名称
+        /// </summary>
+        public string InstituteName { get; set; }
+
+        /// <summary>
+        /// 研究所科系名称
+        /// </summary>
+        public string InstituteDepartmentName { get; set; }
+
+        /// <summary>
+        /// 博士班名称
+        /// </summary>
+        public string DoctoralClassName { get; set; }
+
+        /// <summary>
+        /// 博士班科系名称
+        /// </summary>
+        public string DoctoralDepartmentName { get; set; }
+
+        /// <summary>
+        /// 是否曾经转学
+        /// </summary>
+        public string Transferred { get; set; }
+
+        /// <summary>
+        /// 是否曾经转学 就读说明
+        /// </summary>
+        public string TransferredText { get; set; }
+
+        /// <summary>
+        /// 其他补充
+        /// </summary>
+        public string OtherSupplements { get; set; }
+
+        /// <summary>
+        /// 填写问卷时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+    }
+}
