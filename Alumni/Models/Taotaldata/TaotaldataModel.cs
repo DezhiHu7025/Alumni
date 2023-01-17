3f43727bef53fb05e0a495a3bfbe2e30e131e024
ivy <2023180742@qq.com>
2023-01-17 09:00:38 +0800

    引入Boootstrap;完成注册组数据查询

---
+++ Alumni/Models/Taotaldata/TaotaldataModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alumni.Models.Taotaldata
+{
+    public class TaotaldataModel
+    {
+        /// <summary>
+        /// 流水号
+        /// </summary>
+        public string mchSeqNo { get; set; }
+
+        /// <summary>
+        /// 单据名称
+        /// </summary>
+        public string Form_Name { get; set; }
+
+        /// <summary>
+        /// 审核状态
+        /// </summary>
+        public string IS_PASS { get; set; }
+
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string Stu_Empno { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Stu_Name { get; set; }
+
+        /// <summary>
+        /// 邮件/邮箱地址
+        /// </summary>
+        public string EmailAdress { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 提交时间
+        /// </summary>
+        public string AddTime { get; set; }
+        
+    }
+}
